e10ad76d6b9b86343305f56aa9f9437d47368eb4
DarkDesignX <denis.basler@gmx.ch>
2022-12-19 11:29:17 +0100

    fizzbuzz function

--- cypress/cypress/e2e/unit-tests.cy.js
+++ cypress/cypress/e2e/unit-tests.cy.js
@@ -1,19 +1,24 @@
 /// <reference types="Cypress" />
 
-describe("validateUsername function", () => {
-    before(() => {
+describe("fizzbuzz function", () => {
+    // If you get the error that 'window.fizzbuzz' is undefined, simply replace 'before' with 'beforeeach'
+    beforeEach(() => {
       cy.visit('localhost')
     })
   
-    it('write your tests here', () => {
+    it('write fizz if number is dividible by 3', () => {
       cy.window().then((window) => {
-  
+        const result = window.fizzbuzz(3)
+
+        assert.equal(result, 'Fizz')
+      })
+    })
+
+    it('write buzz if number is dividible by 5', () => {
+      cy.window().then((window) => {
+        const result = window.fizzbuzz(5)
+
+        assert.equal(result, 'Buzz')
       })
     })
   })
-
-
-
-
-
-  
--- webapp/index.php
+++ webapp/index.php
@@ -1,14 +1,20 @@
 <?php $page_title = "Home ★ Productive"; ?>
 <?php require "view/blocks/page_start.php"; ?>
 <h1>Welcome to Productive!</h1>
+<!-- <div class="field">
+	<label for="fizzbuzz-field">Number</label>
+	<input type="number" id="fizzbuzz-field" min="0" required>
+</div> -->
 <?php require "view/blocks/page_end.php"; ?>
 
 <script>
-class UserService {
-  validateUsername = function (username) {
-    // implement validation here
-  }
+window.fizzbuzz = function (number) {
+	//How to return 'fizzbuzz' if the number is devidable by 3 and 5
+	if (number % 3 === 0) {
+		return 'fizz'
+	}
+	if (number % 5 === 0) {
+		return 'buzz'
+	}
 }
-
-window.UserService = UserService;
 </script>
