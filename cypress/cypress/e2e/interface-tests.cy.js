38ce48bd923a26b820a0448277e02599dd9d7ac9
DarkDesignX <denis.basler@gmx.ch>
2023-01-09 14:14:56 +0100

    interface testing

--- cypress/cypress/e2e/interface-tests.cy.js
+++ cypress/cypress/e2e/interface-tests.cy.js
@@ -1,15 +1,42 @@
+const { it } = require("node:test")
+
+const skuCheck = () => {
+    cy.visit('http://localhost/products.php')
+    beforeEach(() => {
+        cy.get('input#username-field').type('root')
+        cy.get('input#password-field').type('sUP3R53CR3T#')
+        cy.get('button[type=submit').click()
+        cy.contains('Products').click()
+    })
+}
+
 describe ("if not signed in", () => {
     it("returns only ACTIVE products on the home page", () => {
-		
-	})
-})
+        cy.get("#products-table tr td:first-child").then(skus => {
+            const activeProducts = products.filter(product => product.active === 1);
+            expect(activeProducts).to.have.length(skus.length);
+        });
+    });
+});
 
 describe ("if signed in", () => {
-	it("returns only ACTIVE products on the home page", () => {
-		
-	})
+    beforeEach(() => {
+        cy.visit('http://localhost')
+        cy.contains('Home').click()
+        cy.get('input#username-field').type('root')
+        cy.get('input#password-field').type('sUP3R53CR3T#')
+        cy.get('button[type=submit').click()
+        cy.contains('Home').click()
+    })
 
-	it('it return ALL products on the products page', () => {
+    it("returns only ACTIVE products on the home page", () => {
+        cy.get("#products-table tr td:first-child").then(skus => {
+            const activeProducts = products.filter(product => product.active === 1);
+            expect(activeProducts).to.have.length(skus.length);
+        });
+    });
+    
+    it('returns ALL products on the products page', () => {
 
-	})
-})
+    });
+});
