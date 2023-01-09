90290e3b5fd966ae9120f27986094348881b02b1
DarkDesignX <denis.basler@gmx.ch>
2023-01-09 13:45:22 +0100

    interface-test

---
+++ cypress/cypress/e2e/interface-tests.cy.js
@@ -0,0 +1,15 @@
+describe ("if not signed in", () => {
+    it("returns only ACTIVE products on the home page", () => {
+		
+	})
+})
+
+describe ("if signed in", () => {
+	it("returns only ACTIVE products on the home page", () => {
+		
+	})
+
+	it('it return ALL products on the products page', () => {
+
+	})
+})
